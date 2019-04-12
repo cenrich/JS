5c0c54afbdf25a3e36f362af4a6e7df184e74ed5
cenrich <ceci_enrich@hotmail.com>
2019-04-11 21:42:05 -0300

    segunda clase

---
+++ clase-dos.js
@@ -0,0 +1,9 @@
+var person1 = ["Lisa", "Simpson", 8]
+var person2 = ["Bart", "Simpson", 10]
+
+var family = {
+    sister:person1,
+    brother:person2
+}
+
+console.log(family.sister[1])
---
+++ index.js
@@ -0,0 +1,20 @@
+var miVariable; /*esto define una variable. la variable se escribe todo junto y no toma guión medio. en este caso, solo estoy reservando el espacio.
+ojo con el punto y coma, que no es lo mismo para js que para css, significa salto de línea*/  
+
+var venue;
+
+venue="IBM";
+
+var generacion="sexta"; /*aquí estoy además de definiendo un espacio, indicando el contenido */
+
+
+//esto imprime en la consola un mensaje. voy a pedir que imprima la variable
+console.log (generacion);
+
+console.log (generacion, venue); /*acá llamé a las dos variables con una coma*/
+
+var impuesto=0.35;
+var sueldo=100000;
+console.log (impuesto*sueldo);
+
+console.log(typeof impuesto);
